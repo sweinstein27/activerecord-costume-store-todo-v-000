e69e2915399b7ef0e837a850f1d2b420097b4d84
Scott Weinstein <sweinstein27@gmail.com>
2018-07-09 14:20:40 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,6 +8,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.boolean :still_in_business
       t.string :opening_time
       t.string :closing_time
+      t.integer :costume_inventory
+      t.integer :num_of_employees
     end
   end
 end
